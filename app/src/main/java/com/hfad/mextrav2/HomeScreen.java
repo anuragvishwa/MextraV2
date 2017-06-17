c471b599e9b005055f314c8929a52e982e271813
satish-gatti <satishgatti@gmail.com>
2017-06-17 17:50:33 +0530

    update

--- app/src/main/java/com/hfad/mextrav2/HomeScreen.java
+++ app/src/main/java/com/hfad/mextrav2/HomeScreen.java
@@ -2,8 +2,12 @@ package com.hfad.mextrav2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class HomeScreen extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
