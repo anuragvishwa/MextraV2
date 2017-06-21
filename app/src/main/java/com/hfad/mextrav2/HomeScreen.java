c1064ba8d5e1849daa5984f3679c9c6b288446ab
satish-gatti <satishgatti@gmail.com>
2017-06-21 10:41:50 +0530

    update work

--- app/src/main/java/com/hfad/mextrav2/HomeScreen.java
+++ app/src/main/java/com/hfad/mextrav2/HomeScreen.java
@@ -1,9 +1,14 @@
 package com.hfad.mextrav2;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 
 import com.hfad.mextrav2.adapter.ContactsAdapter;
@@ -24,15 +29,44 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        recyclerView = (RecyclerView)findViewById(R.id.recylerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        contactList = fetchContactsAll.contacts;
-        adapter = new ContactsAdapter(contactList,this);
-        recyclerView.setAdapter(adapter);
+
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_CONTACTS}, 1);
+
 
 
     }
+
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    recyclerView = (RecyclerView)findViewById(R.id.recylerView);
+                    recyclerView.setHasFixedSize(true);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                    contactList = fetchContactsAll.contacts;
+                    adapter = new ContactsAdapter(contactList,this);
+                    recyclerView.setAdapter(adapter);
+
+                } else {
+
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "You didn't grant permission",
+                            Toast.LENGTH_LONG).show();
+
+                }
+                return;
+            }
+
+        }
+    }
+
+
+
+
+
 
     
 }
