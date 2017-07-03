7a09082972d8f0c412c6f4c3c86ba02de29d3b88
vishwa <anuragcoolkid@gmail.com>
2017-07-03 21:16:07 +0530

    HOME COMMIT

---
+++ app/src/main/java/com/hfad/mextrav2/database/local/DatabaseAdapter.java
@@ -0,0 +1,30 @@
+package com.hfad.mextrav2.database.local;
+
+/**
+ * Created by vishwa on 3/7/17.
+ */
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by vishwa on 3/7/17.
+ */
+
+public class DatabaseAdapter {
+
+    //Create a cursor
+    Context context;
+
+    try{
+        SQLiteOpenHelper mextraDatabaseHelper = new MextraDatabaseHelper(this.context);
+        SQLiteDatabase db = mextraDatabaseHelper.getWritableDatabase();
+        Cursor cursor = db.query("CONTACTS",
+                )
+
+    }
+
+
+}
